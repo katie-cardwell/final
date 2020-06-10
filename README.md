466da64ba466f6eced33300752e69886a371bfcd
katie-cardwell <katie.cardwell@kellogg.northwestern.edu>
2020-06-10 23:29:35 +0000

    Start

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-Describe your project, your motivations behind it, and, 
+Ever want to go on a weekend trip, but you're not sure where to go?  Not sure what to prioritize, if you only have two or three days?
+My site was designed to resolve this porblem.  Travelers can submit destinations optimal for different periods of time.  They can also submit iteneraries to let you know how they focused their short trip.
+Other users can then add reviews, suggestions, or additional tips.
 if hosting your app on Heroku, include your website's live Internet address
--- app.rb
+++ app.rb
@@ -19,3 +19,89 @@ after { puts; }                                                                 
 
 # events_table = DB.from(:events)
 # rsvps_table = DB.from(:rsvps)
+
+destinations_table = DB.from(:destinations)
+itineraries_table = DB.from(itineraries)
+reviews_table = DB.from(reviews)
+users_table = DB.from(:users)
+
+before DO
+    # SELECT * FROM users WHERE id = session[:user_id]
+    @current_user = users_table.where(:id => session[:user_id]).to_a[0]
+    puts @current_user.inspect
+end
+
+# Home page (all destinations)
+get "/" DO
+    # before stuff runs
+    @destinations = destinations_table.all
+    @itineraries = itineraries_table.all
+    puts @destinations.inspect
+    view "destinations"
+end
+
+# Show a single destination
+get "/destinations/:id" DO
+    @users_table = users_table
+    @destination = destinations_table.where(:id => params[:id]).to_a[0]
+    @itinerary = iteneraries_table.where(:destination_id => params["id"]).to_a
+    @average = reviews_table.where(:destination_id => params["id"], :itinerary_id => params["id"]).average
+    view "destination"
+end
+
+# form to create a new itinerary
+get "/destinations/:id/itineraries/new" DO
+    @destination = destinations_table.where(:id => params["id"]).to_a[0]
+    view "new_itinerary"
+end
+
+#Receiving end of new Itinerary form
+post "/destinations/:id/itineraries/create" DO
+    itineraries_table.insert(:destination_id => params["id"],
+                             :days => params["days"],
+                             :user_id => @current_user[:id],
+                             :schedule => params["schedule"])
+    @destination = destinations_table.where(:id => params["id"]).to_a[0]
+    view "create_itinerary"
+end
+
+#Form to create a new user
+get "/users/new" DO
+    view "new_user"
+end
+
+# Receiving end of new user form
+post "/users/create" DO
+    users_table.insert(:name => params["name"],
+                       :email => params["email"],
+                       :password => params["password"])
+    view "create_user"
+end
+
+# form to login
+get "/logins/new" DO
+    view "new_login"
+end
+
+#Receiving end of login form
+post "/logins/create" DO
+    puts params
+    email_entered = params["email"]
+    password_entered = params["password"]
+    user = users_table.where(:email => email_entered).to_a[0]
+    if user
+        puts user.inspect
+        if user[:password] == password_entered
+            session[:user_id] = user[:id]
+            view "create_login"
+        else 
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" DO
+    view "logout"
+end
