891a3deb758067cda9ab16a2b1f86da588f843ec
katie-cardwell <katie.cardwell@kellogg.northwestern.edu>
2020-06-10 23:51:49 +0000

    More updates

--- createdb.rb
+++ createdb.rb
@@ -6,31 +6,36 @@ DB = Sequel.connect(connection_string)                                          
 
 # Database schema - this should reflect your domain model
 # upload with initial seed data for your Places
-DB.create_table! :events do
+DB.create_table! :destinations do
   primary_key :id
   String :title
   String :description, text: true
-  String :date
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :itineraries do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :destination_id
+  foreign_key :user_id
+  String :days
   String :name
   String :email
-  String :comments, text: true
+  String :schedule, text: true
+end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
 end
 
+
 # Insert initial (seed) data
-events_table = DB.from(:events)
+destinations_table = DB.from(:destinations)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+destinations_table.insert(title: "Chicago", 
+                    description: "Chicago is one of the largest cities in the US.  Located right on Lake Michigan, with the Lawrence River running through the middle, the metropolis has beautiful views.  Chicago is known for many things such as its deep-dish pizza, architecture, wind, and more.",
+                    location: "Illinois, USA")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+destinations_table.insert(title: "Montreal", 
+                    description: "If you're looking for a European city, but can't go that far, Montreal is for you.  French-inspired Montreal is a beautiful city with countless activities to fill your weekend.",
+                    location: "Quebec, Canada")
